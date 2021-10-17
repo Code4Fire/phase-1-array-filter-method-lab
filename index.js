ba5f403f6ff604b01b516374a3ae2d862713f40d
Tiffany_Tam <tamtiffany89@gmail.com>
2021-10-17 09:17:44 -0400

    commit tree 24bb8d91ed360346f84dbb1926d807a40fcd4096

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution here
+function findMatching(drivers, obj) {
+  return drivers.filter((element) => {
+    return element.toLowerCase().startsWith(obj.toLowerCase());
+  })
+}
+
+function fuzzyMatch(drivers, obj) {
+  return drivers.filter((element) => {
+    return element.toLowerCase().startsWith(obj.toLowerCase());
+  })
+}
+
+// function matchName(drivers, obj) {
+//   return drivers.filter((element) => {
+//     return element.toLowerCase().startsWith(obj.toLowerCase());
+//   })
+// }
+
+// function matchName(drivers, string) {
+//   return drivers.filter((element) => {
+//     return drivers.name = string 
+//   })
+// }
+//   let matchDriver = drivers.filter(drivers => drivers.name = string)
+//   return matchDriver
+// }
+
+// function matchName(drivers, string) {
+//   let result = drivers.filter(drivers => drivers.name === string)
+//   return result
+// }
+// console.log(matchName(drivers, string))
+
+function matchName(drivers, string) {
+  let result = drivers.filter(drivers => drivers.name === string)
+  return result
+}
+console.log(matchName(drivers, 'Bobby'))
